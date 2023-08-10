1e154d617031f2b59da33b33400bd2548b1acfda
Dhrish Parekh <dhrishparekh.ss@gmail.com>
2023-08-10 23:22:00 +0530

    addded customers to stripe functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { UserButton } from "@clerk/nextjs";
+import Container from "./ui/container";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <Container>
+      <nav className="flex justify-between mx-auto mt-3 items-center ">
+        <div>
+          <h1 className="font-bold text-md ml-2">LOGO</h1>
+        </div>
+        <div>
+          {" "}
+          <div>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+        </div>
+        <div>
+          <ul className="flex items-center space-x-4 mr-2">
+            <li className="cursor-pointer">Features</li>
+            <li className="cursor-pointer">Pricing</li>
+            <li className="cursor-pointer">
+              <Link
+                href={"/dashboard"}
+                className="bg-slate-900 inline-flex items-center justify-center rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-slate-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300 text-slate-50 hover:bg-slate-900/90 dark:bg-slate-50 dark:text-slate-900 dark:hover:bg-slate-50/90 py-2 px-2"
+              >
+                Dashboard
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </Container>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,5 +1,34 @@
-import Stripe from 'stripe'
+import prisma from "@/prisma/client";
+import { auth, currentUser } from "@clerk/nextjs";
+import Stripe from "stripe";
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string,{
-    apiVersion:'2022-11-15'
-})
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: "2022-11-15",
+});
+
+export const createCustomer = async () => {
+  const { userId } = auth();
+  const user = await currentUser();
+
+  if (userId && user) {
+    const User = await prisma.user.findFirst({
+      where: {
+        authId: userId,
+      },
+    });
+    if (!User) {
+      const customer = await stripe.customers.create({
+        email: String(user.emailAddresses[0].emailAddress),
+      });
+      await prisma.user.create({
+        data: {
+          email: user.emailAddresses.map((e) => e.emailAddress),
+          authId: userId,
+          image: user.imageUrl,
+          name: user.firstName?.concat(user.lastName || ""),
+          stripe_cust_id: customer.id,
+        },
+      });
+    }
+  }
+};
